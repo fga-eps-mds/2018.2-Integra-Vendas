4ba203ae2f6ad40699957b99d87e39568c1c87b2
Gustavo Marques <gustavomarques.gml@gmail.com>
2018-09-24 00:41:19 -0300

    Creation of  create_order function
    Co-authored-by: Gustavo Marques <gustavomarques.gml@gmail.com>
    Co-authored-by: Leonardo Medeiros <leonardomedeiros.6@gmail.com>

--- api/api_gateway/api/views.py
+++ api/api_gateway/api/views.py
@@ -15,7 +15,16 @@ import requests
 @api_view(["POST"])
 def delete_product(request):
     try:
-        response = Response(requests.post('http://IP:8002/api/delete_product', data= request.data))
+        response = Response(requests.post('http://192.168.15.9:8002/api/delete_product', data= request.data))
+        return response
+    except:
+        return Response({'error': 'Nao foi possivel se comunicar com o servidor'},
+                                status=HTTP_500_INTERNAL_SERVER_ERROR)
+
+@api_view(["POST"])
+def create_order(request):
+    try:
+        response = Response(requests.post('http://192.168.15.9:8001/api/create_order', data=request.data))
         return response
     except:
         return Response({'error': 'Nao foi possivel se comunicar com o servidor'},

abdced9cacce4e75f2c76954fc1e42b30fd78fd4
Leonardo Medeiros <leonardomedeiros.6@gmail.com>
2018-09-27 03:54:04 -0300

    Added orders_screen function,that returns all orders from an user_id
    Co-authored-by: Gustavo Marques <gustavomarques.gml@gmail.com>
    Co-authored-by: Leonardo Medeiros <leonardomedeiros.6@gmail.com>

--- api/api_gateway/api/urls.py
+++ api/api_gateway/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.conf.urls import url
 from api import views
-from .views import delete_product, create_order
+from .views import delete_product, create_order, orders_screen
 
 urlpatterns = [
     path('api/delete_product', delete_product),
     path('api/create_order', create_order),
+    path('api/orders_screen', orders_screen),
 ]
--- api/api_gateway/api/views.py
+++ api/api_gateway/api/views.py
@@ -10,12 +10,18 @@ from rest_framework.status import (
 )
 from rest_framework.response import Response
 import requests
+import json
+
+host_ip = 'http://192.168.0.10'
+login_microservice_port = ':8000'
+order_microservice_port = ':8001'
+product_microservice_port = ':8002'
 
 # Create your views here.
 @api_view(["POST"])
 def delete_product(request):
     try:
-        response = Response(requests.post('http://192.168.15.9:8002/api/delete_product', data= request.data))
+        response = Response(requests.post(host_ip + product_microservice_port + '/api/delete_product', data= request.data))
         return response
     except:
         return Response({'error': 'Nao foi possivel se comunicar com o servidor'},
@@ -24,8 +30,28 @@ def delete_product(request):
 @api_view(["POST"])
 def create_order(request):
     try:
-        response = Response(requests.post('http://192.168.15.9:8001/api/create_order', data=request.data))
+        response = Response(requests.post(host_ip + order_microservice_port + '/api/create_order', data=request.data))
         return response
     except:
         return Response({'error': 'Nao foi possivel se comunicar com o servidor'},
                                 status=HTTP_500_INTERNAL_SERVER_ERROR)
+
+@api_view(["POST"])
+def orders_screen(request):
+    user_id = request.data.get('user_id')
+    user_products = requests.post(host_ip + product_microservice_port + '/api/user_products', data={'user_id':user_id})
+    #Convert to JSon
+    user_products_response = Response(data=json.loads(user_products.content))
+
+    #List to store all user orders
+    all_user_orders = []
+
+    for product in user_products_response.data:
+        product_orders = requests.post(host_ip + order_microservice_port + '/api/user_orders', data={'product_id':product['id']})
+        orders = json.loads(product_orders.content)
+        for order in orders:
+            if(order['closed'] == False):
+                all_user_orders.append(order)
+
+    response = Response(data=all_user_orders)
+    return response

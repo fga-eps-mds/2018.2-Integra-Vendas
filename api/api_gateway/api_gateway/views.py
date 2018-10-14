36ccc715f06c47a25e7a73a14e52aa00e5b1f77f
Gustavo Marques <gustavomarques.gml@gmail.com>
2018-10-14 11:24:37 -0300

    Adition of api-gatewayversion in json

--- api/api_gateway/api_gateway/views.py
+++ api/api_gateway/api_gateway/views.py
@@ -1,8 +1,32 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+import requests
 
 @api_view(['GET'])
 def status(request):
-    title = "message"
-    message = "Hello for today! See you tomorrow!"
-    return Response({title: message})
+    version=file_get_contents("../VERSION")
+    print (version)
+    return Response({
+    "name":"api-gateway",
+    "version":version,
+    "services":[
+        {
+            "name": "login-microservice",
+            "online": True,
+            "version": "0.1"
+        },
+        {
+            "name": "product-microservice",
+            "online": True,
+            "version": "0.1"
+        },
+        {
+            "name": "order-microservice",
+            "online": False
+        }
+    ],
+})
+
+def file_get_contents(filename):
+    with open(filename) as f:
+        return f.read()

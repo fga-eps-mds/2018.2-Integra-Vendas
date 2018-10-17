cb435d0cbd0f23a3e4fba5943b3f8a6b9c47ad27
Dâmaso Júnio P. Brasileo <junio.pereirab@gmail.com>
2018-10-16 22:56:44 -0300

    Creating methods to change the order status
    Co-authored-by: Dâmaso Júnio Pereira Brasileo <junio.pereirab@gmail.com>
    Co-authored-by: Leonardo Medeiros <leonardomedeiros.6@gmail.com>

--- api/api_gateway/api/views.py
+++ api/api_gateway/api/views.py
@@ -14,6 +14,7 @@ import json
 from django.conf import settings
 from .login_helper import verify_token
 
+ORDER_CLOSED = 1
 # Create your views here.
 @api_view(["POST"])
 def delete_product(request):
@@ -117,7 +118,7 @@ def orders_screen(request):
                                 status=HTTP_500_INTERNAL_SERVER_ERROR)
         orders = product_orders.json()
         for order in orders:
-            if(order['closed'] == False):
+            if(order['status'] != ORDER_CLOSED):
                 all_user_orders.append(order)
 
     return Response(data=all_user_orders, status=HTTP_200_OK)
@@ -149,6 +150,20 @@ def get_name(request):
         response = requests.post(settings.LOGIN + '/api/users/get_name/', data= request.data)
         return Response(data=response.json(), status=response.status_code)
 
+    except:
+        return Response({'error': 'Nao foi possivel se comunicar com o servidor'},
+                                status=HTTP_500_INTERNAL_SERVER_ERROR)
+
+@api_view(["POST"])
+def set_order_status(request):
+    ## Verificação do token
+    verify = verify_token(request.data)
+    if verify.status_code != 200:
+         return verify
+
+    try:
+        response = requests.post(settings.ORDER + '/api/set_order_status/', data= request.data)
+        return Response(response.json())
     except:
         return Response({'error': 'Nao foi possivel se comunicar com o servidor'},
                                 status=HTTP_500_INTERNAL_SERVER_ERROR)

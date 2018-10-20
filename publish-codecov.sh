2b75d72cec947bde898e86f7d205f275e58416a3
Lucas Costa <lucascostaa73@gmail.com>
2018-10-20 03:36:56 -0300

    Updating travis script

--- publish-codecov.sh
+++
@@ -1,5 +0,0 @@
-docker-compose -f $1 build
-docker-compose -f $1 up -d
-docker-compose -f $1 exec api bash -c "sh run-tests.sh"
-docker-compose -f $1 rm -f -s
-cd api/api_gateway && codecov -t ${CODECOV_TOKEN}

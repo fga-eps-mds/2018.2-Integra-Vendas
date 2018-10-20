8475d4e264423a7758e43c7917a9ca9e4353d601
Lucas Costa <lucascostaa73@gmail.com>
2018-10-20 03:10:53 -0300

    Updating publish codecov script

--- publish-codecov.sh
+++ publish-codecov.sh
@@ -1,4 +1,5 @@
 docker-compose -f $1 build
 docker-compose -f $1 up -d
-docker exec api-gateway -e CODECOV_TOKEN=${CODECOV_TOKEN} bash -c "sh run-tests.sh && cd api_gateway && codecov -t ${CODECOV_TOKEN}"
+docker-compose -f $1 exec api bash -c "sh run-tests.sh"
 docker-compose -f $1 rm -f -s
+cd api/api_gateway && codecov -t ${CODECOV_TOKEN}

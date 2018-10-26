de37642a403cc0fb04683e4e69a03278d53c27c5
Shayane Alcantara <shayaneliebe@gmail.com>
2018-10-26 19:35:21 -0300

    Adding test for delete_product function.
    Co-authored-by: Shayane Alcantara <shayaneliebe@gmail.com>
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>

--- api/api_gateway/api/tests/test_product.py
+++ api/api_gateway/api/tests/test_product.py
@@ -8,12 +8,13 @@ default_fk_vendor = 1
 default_price = '10.0'
 default_photo = 'www.google.com'
 default_description = 'description'
+default_product_id = 1
 
 # Create your tests here.
 class ProductTest(TestCase):
     def test_product_creation_with_valid_params(self):
         email = 'teste123@teste.com'
-        
+
         responseJson = registrate_new_user(email)
         loginResponseJson = login_user(email)
 
@@ -41,7 +42,7 @@ class ProductTest(TestCase):
     def test_all_products_with_valid_params(self):
         # product from user 1
         email = 'teste125@teste.com'
-        
+
         responseJson = registrate_new_user(email)
         loginResponseJson = login_user(email)
 
@@ -53,7 +54,7 @@ class ProductTest(TestCase):
 
         # product from user 2
         email2 = 'teste126@teste.com'
-        
+
         responseJson2 = registrate_new_user(email2)
         loginResponseJson2 = login_user(email2)
 
@@ -75,4 +76,39 @@ class ProductTest(TestCase):
     def test_all_products_with_invalid_params(self):
         response = self.client.post('/api/all_products/', data = {'token': None})
 
+        self.assertEqual(response.status_code, 403)
+
+    def test_product_deletion_with_valid_params(self):
+        email = 'teste000@teste.com'
+
+        responseJson = registrate_new_user(email)
+        loginResponseJson = login_user(email)
+
+        login_token = loginResponseJson["token"]
+        fk_vendor = loginResponseJson["user"]["pk"]
+
+        data = create_product(default_name, fk_vendor, default_price, default_photo, default_description, login_token)
+        response = self.client.post('/api/create_product/', data=data)
+
+        data2 = {'user_id': fk_vendor,'token': login_token}
+        response2 = self.client.post('/api/my_products_screen/', data=data2)
+
+        data3 = {
+            'fk_vendor': fk_vendor,
+            'product_id': response2.data[0]["id"],
+            'token': login_token
+        }
+
+        response3 = self.client.post('/api/delete_product/', data=data3)
+
+        self.assertEqual(response3.status_code, 200)
+
+    def test_all_products_with_invalid_params(self):
+        data = {
+            'fk_vendor': default_fk_vendor,
+            'product_id': default_product_id,
+            'token': None
+        }
+
+        response = self.client.post('/api/delete_product/', data=data)
         self.assertEqual(response.status_code, 403)

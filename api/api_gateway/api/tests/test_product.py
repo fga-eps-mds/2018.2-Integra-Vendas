e1af39536935d057ba352f78d1dba7507891ca53
Welison Lucas <welison.almeida.923@gmail.com>
2018-10-26 19:37:25 -0300

    Creating test to my products screen method.
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>
    
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>
    Co-authored-by: Shayane Alcantara <shayaneliebe@gmail.com>

--- api/api_gateway/api/tests/test_product.py
+++ api/api_gateway/api/tests/test_product.py
@@ -76,3 +76,33 @@ class ProductTest(TestCase):
         response = self.client.post('/api/all_products/', data = {'token': None})
 
         self.assertEqual(response.status_code, 403)
+
+    def test_my_products_screen_with_valid_params(self):
+        # product from user 1
+        email = 'teste127@teste.com'
+        
+        responseJson = registrate_new_user(email)
+        loginResponseJson = login_user(email)
+
+        fk_vendor = loginResponseJson["user"]["pk"]
+        login_token = loginResponseJson["token"]
+
+        data = create_product(default_name, fk_vendor, default_price, default_photo, default_description, login_token)
+        response = self.client.post('/api/create_product/', data=data)
+
+        data2 = create_product(default_name, fk_vendor, default_price, default_photo, default_description, login_token)
+        response2 = self.client.post('/api/create_product/', data=data2)
+
+        # retriving all products
+        data3 = {'user_id': fk_vendor,'token': login_token}
+        response3 = self.client.post('/api/my_products_screen/', data=data3)
+
+        self.assertEqual(response3.data[0]["fk_vendor"], fk_vendor)
+        self.assertEqual(response3.data[1]["fk_vendor"], fk_vendor)
+        self.assertEqual(response3.status_code, 200)
+
+    def test_my_products_screen_with_invalid_params(self):
+        data = {'user_id': default_fk_vendor, 'token': None}
+        response = self.client.post('/api/my_products_screen/', data=data)
+
+        self.assertEqual(response.status_code, 403)

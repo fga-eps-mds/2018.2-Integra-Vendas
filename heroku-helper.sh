9cded2b022df589853afdf1cacc99a4528a31efb
Lucas Costa <lucascostaa73@gmail.com>
2018-10-26 03:24:12 -0300

    Creating script to heroku update version before deploy

---
+++ heroku-helper.sh
@@ -0,0 +1,8 @@
+VERSION=$(cat ./api/VERSION)
+# echo ${VERSION}
+
+curl -n -X PATCH https://api.heroku.com/apps/${HEROKU_APP}/config-vars \
+  -d "{ \"VERSION\":\"${VERSION}\" }" \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/vnd.heroku+json; version=3" \
+  -H "Authorization: Bearer ${HEROKU_DEV_KEY}"

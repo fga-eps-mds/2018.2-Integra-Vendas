e94bef03fdf88df6b3e4064a501d47a113cf5324
Gustavo Marques <gustavomarques.gml@gmail.com>
2018-11-13 01:01:49 -0200

    Correction of error
    Co-authored-by: Gustavo Marques <gustavomarques.gml@gmail.com>
    Co-authored-by: André Lucas de Sousa Pinto <andrelucax@gmail.com>

--- api/api_gateway/api_gateway/version_helper.py
+++ api/api_gateway/api_gateway/version_helper.py
@@ -6,9 +6,9 @@ from django.conf import settings
 def get_version(name):
     general_timeout = 2
     url = get_url(name)
-    if(url == None) 
+    if(url == None):
         return
-        
+
     try:
         response = requests.get(url, timeout=general_timeout)
         response_json=response.json()
@@ -19,7 +19,7 @@ def get_version(name):
         }
     return response_json
 
-def get_url(name)
+def get_url(name):
     if name == "login-microservice":
         url = settings.LOGIN
     elif name == "product-microservice":

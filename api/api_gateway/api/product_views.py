8ddad88216eda5f521d1d3524af8c408044bc13f
Leonardo Medeiros <leonardomedeiros.6@gmail.com>
2018-11-16 20:29:48 -0200

    Solving Cognivite Complexity and Duplications in product_views
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>
    Co-authored-by: Leonardo Medeiros <leonardomedeiros.6@gmail.com>

--- api/api_gateway/api/product_views.py
+++ api/api_gateway/api/product_views.py
@@ -9,11 +9,8 @@ from .login_views import verify_token
 
 @api_view(["POST"])
 def create_product(request):
-    verify = verify_token(request.data)
-    if verify.status_code != 200:
-        return verify
-
     DEFAULT_PRODUCT_IMAGE = 'https://res.cloudinary.com/integraappfga/image/upload/v1541537829/senk2odnxamopwlkmyoq.png'
+    
     product = request.data.dict()
     photo = product.get("photo")
     if (photo != None):
@@ -26,16 +23,7 @@ def create_product(request):
     else:
         product.update({'photo': DEFAULT_PRODUCT_IMAGE})
     
-    try:
-        response = requests.post(settings.PRODUCTS + '/api/create_product/', data=product)
-        try:
-            response_json = response.json()
-            return Response(data=response_json, status=response.status_code)
-        except:
-            return Response(response)
-    except:
-        return Response({'error': 'Não foi possível se comunicar com o servidor.'},
-                                status=HTTP_500_INTERNAL_SERVER_ERROR)
+    return default_response(settings.PRODUCTS + '/api/create_product/', product)
 
 
 @api_view(["POST"])
@@ -52,10 +40,6 @@ def get_product(request):
 
 @api_view(["POST"])
 def edit_product(request):
-    verify = verify_token(request.data)
-    if verify.status_code != 200:
-        return verify
-
     # Transforming request to python dictionary to treat photo
     product = request.data.dict()
     photo = product.get("photo")
@@ -69,15 +53,7 @@ def edit_product(request):
 
     # If there is no photo in request just do not send it to the microservice
 
-    try:
-        response = requests.post(settings.PRODUCTS + '/api/edit_product/', data=product)
-        try:
-            response_json = response.json()
-            return Response(data=response_json)
-        except:
-            return Response(response)
-    except:
-        return Response({'error': 'Nao foi possivel se comunicar com o servidor'},status=HTTP_500_INTERNAL_SERVER_ERROR)
+    return default_response(settings.PRODUCTS + '/api/edit_product/', product)
 
 @api_view(["POST"])
 def delete_product(request):
